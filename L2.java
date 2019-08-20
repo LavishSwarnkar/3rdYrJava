b53d52c556d2173104d638ea2eb44d0cc19c310d
Lavish Swarnkar <lavishswarnkar@gmail.com>
2019-08-20 12:11:22 +0530

    Add files via upload

---
+++ L2.java
@@ -0,0 +1,8 @@
+// 2. Addition of 2 vars
+import java.util.*;
+class L2{
+	public static void main(String args[]){
+		int a=958, b=9043;
+		System.out.println(a+b); //String.format("%d + %d = %d", a, b, a+b)
+	}
+}
